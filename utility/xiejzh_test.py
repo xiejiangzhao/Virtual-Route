536ce69632547dc95759d007170983edfc43a321
xiejiangzhao <xiejzh3@mail2.sysu.edu.cn>
2018-05-16 00:06:37 +0800

    修复了DV部分,加入测试模块应该没有问题了

---
+++ utility/xiejzh_test.py
@@ -0,0 +1,21 @@
+from utility.RouteTable import RouteTable
+from utility.DVTable import DVTable
+import json
+
+if __name__ == '__main__':
+    Aroute = RouteTable('config/Aroute.json')
+    Broute = RouteTable('config/Broute.json')
+    Croute = RouteTable('config/Croute.json')
+    Droute = RouteTable('config/Droute.json')
+    Eroute = RouteTable('config/Eroute.json')
+    Froute = RouteTable('config/Froute.json')
+    ADV = DVTable('config/ADV.json', Aroute,'127.0.0.1',9000)
+    BDV = DVTable('config/BDV.json', Broute,'127.0.0.1',9001)
+    CDV = DVTable('config/CDV.json', Croute,'127.0.0.1',9002)
+    DDV = DVTable('config/DDV.json', Droute,'127.0.0.1',9003)
+    EDV = DVTable('config/EDV.json', Eroute,'127.0.0.1',9004)
+    FDV = DVTable('config/FDV.json', Froute,'127.0.0.1',9005)
+    BDV.update_table_by_table('127.0.0.1', 9000, ADV.DVTable)
+    ADV.update_table_by_table('127.0.0.1', 9001, BDV.DVTable)
+    CDV.update_table_by_table('127.0.0.1', 9001, BDV.DVTable)
+    DDV.update_table_by_table('127.0.0.1', 9001, BDV.DVTable)
