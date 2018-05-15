f6b250f44f00869b711436cb753785d13d01fba4
weiby3 <393245304@qq.com>
2018-05-15 18:06:44 +0800

    Update RouteRequestHandler and add new functions.

--- utility/RouteServer.py
+++ utility/RouteServer.py
@@ -1,19 +1,30 @@
 import socketserver
 import json
+import socket
 
 
 def generate_response(**kwargs) -> bytes:
     b = json.dumps(kwargs, ensure_ascii=False) + "\n"
-    return f"{len(b):08X}{b}".encode("UTF-8")
+    # return f"{len(b):08X}{b}".encode("UTF-8")
+    return b.encode("UTF-8")
 
 
-class RouteServer(socketserver.ThreadingTCPServer):
-    def __init__(self, server_address, RequestHandlerClass, route_table):
-        super().__init__(server_address, RequestHandlerClass)
-        self.route_table = route_table
+def send_message(data: bytes, ip: str, port: int) -> None:
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip, port))
+        s.sendall(data)
+        rc = str(s.recv(1024), 'UTF-8')
+        print(rc)
 
 
 class RouteRequestHandler(socketserver.StreamRequestHandler):
+    """
+     * function need to implement:
+     * update_route(dict) -> None
+     * is_destination(str, int) -> bool
+     * get_next_hop(str, int) -> Tuple[str, int]
+     * get_route(str, int) -> dict
+    """
     def handle(self):
         while True:
             try:
@@ -33,5 +44,16 @@ class RouteRequestHandler(socketserver.StreamRequestHandler):
             if parsed_json["type"] == "update_route":
                 update_route(parsed_json["data"])
                 self.wfile.write(generate_response(code=200))
+                print(f"UPDATE_ROUTE from {parsed_json['src_ip']}:{parsed_json['src_port']}")
             elif parsed_json["type"] == "message":
-                pass
+                if is_destination(parsed_json["dst_ip"], parsed_json["dst_port"]):
+                    print("RECV:", parsed_json["data"])
+                else:
+                    ip, port = get_next_hop(parsed_json["dst_ip"], parsed_json["dst_port"])
+                    send_message(json.dumps(parsed_json, ensure_ascii=False).encode("UTF-8"), ip, port)
+                    print("NEXT_HOP:", ip, port)
+            elif parsed_json["type"] == "get_route_from_controller":
+                ip, port = parsed_json["data"]
+                rd = get_route(ip, port)
+                send_message(json.dumps(rd, ensure_ascii=False).encode("UTF-8"), ip, port)
+                print(f"SEND_ROUTE to {ip}:{port}.")
