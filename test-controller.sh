1b737eeaa47744a202c712797359910a7be495d6
weiby3 <393245304@qq.com>
2018-05-17 12:49:13 +0800

    Update test-controller.sh.

--- test-controller.sh
+++ test-controller.sh
@@ -10,22 +10,22 @@ tmux split-window -h -p 50 -t "test-controller":0.0
 tmux split-window -v -p 50 -t "test-controller":0.0
 tmux split-window -v -p 50 -t "test-controller":0.1
 
-tmux send -t "test-controller":0.0 "$PYTHON $SRC/controller_client.py" Enter
+tmux send -t "test-controller":0.3 "$PYTHON $SRC/controller_client.py" Enter
 tmux send -t "test-controller":0.1 "$PYTHON $SRC/controller_client.py" Enter
 tmux send -t "test-controller":0.2 "$PYTHON $SRC/controller_client.py" Enter
-tmux send -t "test-controller":0.3 "$PYTHON $SRC/controller.py" Enter
+tmux send -t "test-controller":0.0 "$PYTHON $SRC/controller.py" Enter
 
 tmux send -t "test-controller":0.1 '1' Enter
 tmux send -t "test-controller":0.1 'n' Enter
 tmux send -t "test-controller":0.2 '2' Enter
 tmux send -t "test-controller":0.2 'n' Enter
-tmux send -t "test-controller":0.0 '0' Enter
-tmux send -t "test-controller":0.0 'y' Enter
-tmux send -t "test-controller":0.0 'Hello, World!' Enter
-tmux send -t "test-controller":0.0 '127.0.0.1' Enter
-tmux send -t "test-controller":0.0 '9002' 
+tmux send -t "test-controller":0.3 '0' Enter
+tmux send -t "test-controller":0.3 'y' Enter
+tmux send -t "test-controller":0.3 'Hello, World!' Enter
+tmux send -t "test-controller":0.3 '127.0.0.1' Enter
+tmux send -t "test-controller":0.3 '9002'
 
-tmux choose-window -t "test-controller":0.0
+# tmux choose-window -t "test-controller":0.0
 
 # cd ..
 
