61c128ea4208174ca7cca0ae853debc9db1a9c6c
weiby3 <393245304@qq.com>
2018-05-18 18:34:11 +0800

    Modify dv_router.py

--- dv_router.py
+++ dv_router.py
@@ -1,19 +1,28 @@
 import logging
-import coloredlogs
+# import coloredlogs
 import socketserver
 import socket
 import json
+import threading
+from time import sleep
+from utility.RouteTable import RouteTable
+from utility.DVTable import DVTable
+from os import path
+
+# coloredlogs.install()
+logging.basicConfig(format='[%(asctime)s %(name)s %(levelname)s]:\n%(message)s\n')
 
 
-coloredlogs.install()
-logging.basicConfig(format='[%(asctime)s - %(name)s - %(levelname)s - %(ip)s:%(port)d] : \n%(message)s\n')
-
+# formatter = logging.Formatter('[%(asctime)s %(name)s %(ip)s:%(port)d %(levelname)s]:\n%(message)s\n')
+# console_handler = logging.StreamHandler()
+# console_handler.setFormatter(formatter)
 
 
 def send_bytes(s: socket.socket, data: bytes) -> None:
     l = len(data)
     ln = f"{l:08X}".encode()
     s.sendall(ln + data)
+
 
 def recv_bytes(s: socket.socket) -> bytes:
     l = s.recv(8)
@@ -25,8 +34,16 @@ def recv_bytes(s: socket.socket) -> bytes:
         data += s.recv(l - len(data))
     return data
 
+
 def send_dict(s: socket.socket, **kwargs) -> None:
     return send_bytes(s, json.dumps(kwargs, ensure_ascii=False).encode("UTF-8"))
+
+
+def send_dict_and_recv_bytes(ip: str, port: int, **kwargs):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((ip, port))
+        send_bytes(s, json.dumps(kwargs, ensure_ascii=False).encode("UTF-8"))
+        return recv_bytes(s)
 
 
 class DVRouteRequestHandler(socketserver.StreamRequestHandler):
@@ -38,14 +55,21 @@ class DVRouteRequestHandler(socketserver.StreamRequestHandler):
         try:
             parsed_json = json.loads(raw_data, encoding="UTF-8")
         except Exception as e:
-            logger.error(f"Parsed to json error.\nraw_data={raw_data}\n{e}")
+            sip, sport = sk.getpeername()
+            self.logger.error(f"Parsed to json error.\nFrom {sip}:{sport}\nraw_data={raw_data}\n{e}")
             return
+        sip, sport = parsed_json["src_ip"], parsed_json["src_port"]
+        dip, dport = parsed_json["dst_ip"], parsed_json["dst_port"]
         if parsed_json["type"] == "heart":
+            self.logger.info(f"Receive heart ping from {sip}:{sport}")
             send_dict(sk, code=200)
         elif parsed_json["type"] == "message":
             pass
         elif parsed_json["type"] == "update_route":
-            pass
+            dvt.update_table_by_table(sip, sport, parsed_json["data"])
+            send_dict(sk, code=200)
+            if dvt.changed:
+                pass
         sk.close()
 
 
@@ -55,11 +79,36 @@ ROUTER_PORTS = [9000, 9001, 9002, 9003, 9004, 9005]
 
 self_ip = ""
 self_port = 0
+rt: RouteTable = None
+dvt: DVTable = None
+
+
+def start_server(ip: str, port: int) -> None:
+    ss = socketserver.ThreadingTCPServer((ip, port), DVRouteRequestHandler)
+    logger = logging.getLogger(__name__)
+    logger.info(f"Prepare to start RouteServer({ip}:{port})...")
+    try:
+        ss.serve_forever()
+    except KeyboardInterrupt:
+        logger.info(f"Prepare to stop RouteServer({ip}:{port})...")
+        exit(0)
+
+
+def heart_loop(rt: RouteTable, dvt: DVTable) -> None:
+    while True:
+        for i in rt.get_neibour():
+            pass
+
 
 if __name__ == "__main__":
     cid = int(input(f"Please input the client ID(0-{len(ROUTER_PORTS) - 1}):\n"))
     self_ip = ROUTER_IP
     self_port = ROUTER_PORTS[cid]
+    rt = RouteTable(path.join(".", "utility", "config", f"{ROUTERS[cid]}route.json"))
+    dvt = DVTable(path.join(".", "utility", "config", f"{ROUTERS[cid]}DV.json"), rt, self_ip, self_port)
+    server_thread = threading.Thread(target=start_server, args=(self_ip, self_port))
+    server_thread.start()
+
     hm = ""
     while True:
         if hm == "y" or hm == "n":
@@ -67,5 +116,6 @@ if __name__ == "__main__":
         hm = input("Would you like to send message? y/n ")
     if hm == "y":
         ms = input("Please input your message:\n")
-        dip = input("Please input destination ip:\n")
-        dport = int(input("Please input destination port:\n"))
+        did = int(input(f"Please input destination ID(0-{len(ROUTER_PORTS) - 1}):\n"))
+        dip = ROUTER_IP
+        dport = ROUTER_PORTS[did]
